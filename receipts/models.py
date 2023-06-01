ab5fadfb95736a81b518c80d2d249ca0e9bcf267
Justin Huynh <justinhuynh639@gmail.com>
2023-06-01 14:28:37 -0700

    Feature 5 complete

--- receipts/models.py
+++ receipts/models.py
@@ -8,8 +8,11 @@ class ExpenseCategory(models.Model):
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         related_name="categories",
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
+
+    def __str__(self):
+        return self.name
 
 
 class Account(models.Model):
@@ -18,34 +21,40 @@ class Account(models.Model):
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         related_name="accounts",
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
+
+    def __str__(self):
+        return self.name
 
 
 class Receipt(models.Model):
     vendor = models.CharField(max_length=200)
     total = models.DecimalField(
         decimal_places=3,
-        max_digits=10
+        max_digits=10,
     )
     tax = models.DecimalField(
         decimal_places=3,
-        max_digits=10
+        max_digits=10,
     )
     date = models.DateTimeField()
     purchaser = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         related_name="receipts",
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
     category = models.ForeignKey(
         "ExpenseCategory",
         related_name="receipts",
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
     )
     account = models.ForeignKey(
         "Account",
         related_name="receipts",
         on_delete=models.CASCADE,
-        null=True
+        null=True,
     )
+
+    def __str__(self):
+        return self.vendor
